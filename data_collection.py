1954ee705e38f2f351f6b2d09646f0cbdccc2931
Rohan <rohanaras@gmail.com>
2016-01-03 17:49:36 -0800

    set up basic collection structure

---
+++ data_collection.py
@@ -0,0 +1,51 @@
+def getJson(suffix):
+	import urllib, json
+	urlObject = urllib.urlopen('http://api.pugetsound.onebusaway.org/api/where/' + suffix)
+	return json.loads(urlObject.read())
+
+def testSQL(cnx):
+	from datetime import datetime, date
+	cursor = cnx.cursor()
+	test_insert = ("INSERT INTO test (word, number, date) "
+				   "VALUES (%(word_entry)s, %(number_entry)s, %(date_entry)s)")
+	test_data = {
+		'word_entry': 'TestData',
+		'number_entry':17,
+		'date_entry':datetime.now().date()
+	}
+	cursor.execute(test_insert, test_data)
+	cnx.commit()
+
+
+def apiRequest():
+	print('dummy')
+
+def inputData():
+	'''
+	currently puts data in a localhost db
+	'''
+	import mysql.connector
+
+	config = {
+		'user':'testUser',
+		'password':'anything',
+    	'host':'127.0.0.1',
+    	'database':'TestOBA'
+	}
+
+	try:
+		cnx = mysql.connector.connect(**config)
+		testSQL(cnx)
+	except mysql.connector.Error as err:
+		if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+			print("Something is wrong with your user name or password")
+		elif err.errno == errorcode.ER_BAD_DB_ERROR:
+			print("Database does not exist")
+		else:
+			print(err)
+	else:
+		cnx.close()
+
+apiRequest
+if __name__ == "__main__":
+	inputData()
